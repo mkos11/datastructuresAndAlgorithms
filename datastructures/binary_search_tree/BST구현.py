26163577b648bd7a0aa726c19be97d3e24d4d2fc
jonghyeok <alb7979s@naver.com>
2021-02-12 12:08:20 +0900

    명절맞이 자료구조 구현

--- datastructures/binary_search_tree/BST구현.py
+++ datastructures/binary_search_tree/BST구현.py
@@ -1,3 +1,4 @@
+from collections import*
 class Node:
     def __init__(self, data):
         self.data = data
@@ -21,13 +22,16 @@ class BST:
         else:
             node.right = self._push(node.right, data)
         return node
-    # pop에서도 find랑 연산 비슷한데 합칠수도 있을듯?, 찾으면 node 리턴하고, 없으면 None 리턴
+
     def pop(self, data):
-        return self._pop(self.find(data))
+        self.root, deletedNode = self._pop(self.root, data)
+        return deletedNode
     # 양쪽 자식 있으면 오른쪽 서브트리 중 가장 작은거 삭제해줄거
-    def _pop(self, node):
+    def _pop(self, node, data):
         deletedNode = None
-        if node is not None:
+        if node is None:
+            return node, deletedNode
+        if node.data == data:
             deletedNode = node
             if node.left and node.right:
                 parent, child = node, node.right
@@ -45,7 +49,11 @@ class BST:
                 node = node.right
             else:
                 node = None
-        return deletedNode
+        elif node.data < data:
+            node.right, deletedNode = self._pop(node.right, data)
+        else:
+            node.left, deletedNode = self._pop(node.left, data)
+        return node, deletedNode
 
 
     def find(self, data):
@@ -59,16 +67,67 @@ class BST:
             return self._find(node.left, data)
 
     # 순회도 추가하기 (preorder, inoredr, postorder, levelorder)
+    def preorder(self):
+        print("pre-order")
+        def _preorder(node):
+            if node is None: return
+            print(node.data, end=' ')
+            _preorder(node.left)
+            _preorder(node.right)
+        _preorder(self.root)
+        print()
+
+    def inorder(self):
+        print("in-order")
+        def _inorder(node):
+            if node is None: return
+            _inorder(node.left)
+            print(node.data, end=' ')
+            _inorder(node.right)
+        _inorder(self.root)
+        print()
+
+    def postorder(self):
+        print("post-order")
+        def _postorder(node):
+            if node is None: return
+            _postorder(node.left)
+            _postorder(node.right)
+            print(node.data, end=' ')
+        _postorder(self.root)
+        print()
+
+    # split = True면 층별로 출력
+    def levelorder(self, split=False):
+        print("level-order")
+        def _levelorder(node):
+            q = deque([node])
+            while q:
+                lq = len(q)
+                for i in range(lq):
+                    node = q.popleft()
+                    if node is not None:
+                        print(node.data, end=' ')
+                        if node.left: q.append(node.left)
+                        if node.right: q.append(node.right)
+                if split: print()
+        _levelorder(self.root)
+        print()
 
 if __name__ == "__main__":
     # test
-    arr = [3 ,1 ,5, -32, 61, 223, 41, 53, 62]
+    arr = [3, 1, 5, -32, 61, 223, 41, 53, 62]
     bst = BST()
     for x in arr: bst.push(x)
+    bst.levelorder()
+    # bst.levelorder(True)
+    bst.preorder()
+    bst.inorder()
+    bst.postorder()
     print(bst.find(1))
     print(bst.find(2))
     
-    # pop() 문제있는데? 다시 고치기
     print(bst.pop(1))
     print(bst.pop(1))
+
 
