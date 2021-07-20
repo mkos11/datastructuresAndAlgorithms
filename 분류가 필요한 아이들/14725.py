e671c0ca0be6fbf92abfa9f1e6c9f01071a976bf
김종혁 <alb7979s@naver.com>
2021-07-20 23:11:01 +0900

    스터디 문제풀이

---
+++ 분류가 필요한 아이들/14725.py
@@ -0,0 +1,28 @@
+# https://www.acmicpc.net/problem/14725
+# Trie
+from sys import*
+input = lambda: stdin.readline().rstrip()
+class Node:
+    def __init__(self):
+        self.child = {}
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, params):
+        node = self.root
+        for param in params:
+            if param not in node.child:
+                node.child[param] = Node()
+            node = node.child[param]
+
+    def draw(self, node, depth):
+        for child in sorted(node.child):
+            print(('--'*depth) + child)
+            self.draw(node.child[child], depth+1)
+
+n = int(input())
+trie = Trie()
+for i in range(n):
+    trie.insert(list(map(str, input().split()))[1:])
+trie.draw(trie.root, 0)
