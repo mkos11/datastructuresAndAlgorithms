2c52f8de1b6919ada551eb2395a9ecef041f780e
김종혁 <alb7979s@naver.com>
2021-08-02 22:50:36 +0900

    스터디 문제풀이

--- 분류가 필요한 아이들/programmers_N으로 표현.py
+++ 분류가 필요한 아이들/programmers_N으로 표현.py
@@ -1,27 +1,23 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42895
-from collections import*
-def bfs(numbers, target):
-    visit = {}
-    q = deque()
-    for number, cnt in numbers:
-        visit[number] = cnt
-        q.append(number)
-    while q:
-        u = q.popleft()
-        if u == target: return visit[u]
-        # +, -, *, // 네가지 연산을
-        # numbers에 대해 모두 수행
-        for number, cnt in numbers:
-            if visit[number] + cnt > 8: break
-            for v in [u + number, u * number, u // number, u - number]:
-                if v in visit or v > target * 10: continue
-                visit[v] = visit[u] + cnt
-                q.append(v)
-    return -1
-
-def solution(n, target):
-    numbers = []
-    for i in range(1, len(str(target))+1):
-        numbers.append([int(str(n)*i), i])
-    return bfs(numbers, target)
-print(solution(5, 12))
+INF = int(1e9)
+n, num = 0, 0
+def solution(N, number):
+    global n, num
+    n = N
+    num = number
+    res = dfs(0, 0)
+    return res if res != INF else -1
+def dfs(number, cnt):
+    res = INF
+    if cnt > 8: return INF
+    if number == num: return cnt
+    temp = 0
+    for i in range(1, 9):
+        temp = temp * 10 + n
+        res = min(res, dfs(number + temp, cnt + i))
+        res = min(res, dfs(number * temp, cnt + i))
+        res = min(res, dfs(number // temp, cnt + i))
+        res = min(res, dfs(number - temp, cnt + i))
+    return res
+# print(solution(	5, 31168))
+print(solution(	5, 12))
