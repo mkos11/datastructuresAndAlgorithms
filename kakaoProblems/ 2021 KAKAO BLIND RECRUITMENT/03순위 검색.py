7eb5a99342ca70d5535d8f751247b0887c3a0ff8
김종혁 <alb7979s@naver.com>
2021-08-11 22:56:30 +0900

    카카오 기출 문제풀이

--- kakaoProblems/ 2021 KAKAO BLIND RECRUITMENT/03순위 검색.py
+++ kakaoProblems/ 2021 KAKAO BLIND RECRUITMENT/03순위 검색.py
@@ -6,18 +6,46 @@ class Trie:
     def __init__(self):
         self.root = Node()
     def insert(self, node, pos, info):
+        if pos == len(info) - 1:
+            if 'score' not in node.child:
+                node.child['score'] = []
+            node.child['score'].append(int(info[pos]))
+            return
         if info[pos] not in node.child:
             node.child[info[pos]] = Node()
         self.insert(node.child[info[pos]], pos+1, info)
         if '-' not in node.child:
             node.child['-'] = Node()
         self.insert(node.child['-'], pos+1, info)
-#       pos basecase 잡아서 숫자 처리해줘야함, 나중에 숫자 배열 정렬도 생각해줘야함
+    def sort_score(self, node):
+        if not node: return
+        for k, v in node.child.items():
+            if k == 'score':
+                v.sort()
+            else:
+                self.sort_score(v)
+    def search(self, node, pos, info):
+        if pos == len(info) - 1:
+            return self.binary_search(node.child['score'], int(info[pos]))
+        if info[pos] not in node.child: return 0
+        return self.search(node.child[info[pos]], pos+1, info)
+    def binary_search(self, arr, target):
+        l, r = 0, len(arr)-1
+        while l <= r:
+            m = (l + r) // 2
+            if arr[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return len(arr) - l
 def solution(info, query):
     trie = Trie()
     for x in info:
         trie.insert(trie.root, 0, list(x.split()))
+    trie.sort_score(trie.root)
     answer = []
+    for x in query:
+        answer.append(trie.search(trie.root, 0, x.replace('and', '').split()))
     return answer
 
 print(solution(["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"], ["java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"]))
