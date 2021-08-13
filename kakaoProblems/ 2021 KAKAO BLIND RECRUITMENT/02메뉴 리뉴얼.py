a552d35c38ced3c4e19d95fc129f46a11804b567
김종혁 <alb7979s@naver.com>
2021-08-13 23:28:03 +0900

    카카오 기출 풀이

--- kakaoProblems/ 2021 KAKAO BLIND RECRUITMENT/02메뉴 리뉴얼.py
+++ kakaoProblems/ 2021 KAKAO BLIND RECRUITMENT/02메뉴 리뉴얼.py
@@ -1,5 +1,24 @@
 # https://programmers.co.kr/learn/courses/30/lessons/72411
-
+# 시간초과 날거 같은데????????? 한번 일단 풀어보기 난다 => 담부터 완전 잘 보고 풀기, 안난다 => 담부터 낮은 단계는 일단 풀기
+from itertools import*
+def check(total, order):
+    for t in total:
+        if t not in order: return False
+    return True
 def solution(orders, course):
-    answer = []
-    return answer
+    total = set()
+    for order in orders:
+        for c in course:
+            for x in combinations(order, c):
+                total.add(x)
+    ans = set()
+    for t in total:
+        cnt = 0
+        for order in orders:
+            if check(t, order): cnt += 1
+            if cnt >= 2:
+                ans.add(t)
+                break
+    print(ans)
+
+print(solution(	["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]))
