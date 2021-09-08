e4d103fb6179b669dcbf457e20a5f8e22a6378ba
김종혁 <alb7979s@naver.com>
2021-09-08 21:31:14 +0900

    스터디 문제 풀이

--- 분류가 필요한 아이들/22945_팀 빌딩.py
+++ 분류가 필요한 아이들/22945_팀 빌딩.py
@@ -1,1 +1,13 @@
 # https://www.acmicpc.net/problem/22945
+n = int(input())
+arr = list(map(int, input().split()))
+l, r = 0, n-1
+res = 0
+while l < r:
+    # print(l, r, res)
+    res = max(res, (r-l-1) * min(arr[l], arr[r]))
+    if arr[l] < arr[r]:
+        l += 1
+    else:
+        r -= 1
+print(res)
--- 분류가 필요한 아이들/풍선 터트리기.py
+++ 분류가 필요한 아이들/풍선 터트리기.py
@@ -1,4 +1,21 @@
 # https://programmers.co.kr/learn/courses/30/lessons/68646
-def solution(a):
-    answer = 0
-    return answer
+INF = int(1e9)
+def solution(arr):
+    n = len(arr)
+    MIN = INF
+    less_than = [0] * n
+    for i in range(n):
+        if MIN > arr[i]:
+            MIN = arr[i]
+        else:
+            less_than[i] += 1
+    MIN = INF
+    for i in range(n-1, -1, -1):
+        if MIN > arr[i]:
+            MIN = arr[i]
+        else:
+            less_than[i] += 1
+    # print(less_than)
+    return len(list(filter(lambda x: x != 2, less_than)))
+
+print(solution([-16, 27, 65, -2, 58, -92, -71, -68, -61, -33]))
